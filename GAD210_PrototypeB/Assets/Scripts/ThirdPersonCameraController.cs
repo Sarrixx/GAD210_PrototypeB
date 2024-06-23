cb7cf40bd2e7c7f2792363318c0013a518e3aa53
Sarrixx <joshua.fizz.ferguson@gmail.com>
2024-06-24 02:07:39 +0800

    - Modified camera controller to fade out walls (but was having bugs with the fade render mode for materials) - Setup first (bright) and second (dim) rooms - Implemented night ambience - Created materials for collectibles

--- GAD210_PrototypeB/Assets/Scripts/ThirdPersonCameraController.cs
+++ GAD210_PrototypeB/Assets/Scripts/ThirdPersonCameraController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ThirdPersonCameraController : MonoBehaviour
@@ -7,6 +9,8 @@ public class ThirdPersonCameraController : MonoBehaviour
     [SerializeField] private float rotationTime = 10f;
     [SerializeField] private float moveTime = 10f;
     [SerializeField] private float scrollSensitivity = 10f;
+    [SerializeField] private float fadeTime = 0.5f;
+    [SerializeField] private LayerMask boundsMask;
     [SerializeField] private Vector2 zClamp = new Vector2(1, 10f);
     [SerializeField] private bool slerpMovement = false;
     [SerializeField] private bool slerpRotation = false;
@@ -17,6 +21,7 @@ public class ThirdPersonCameraController : MonoBehaviour
     private Vector3 moveLerpTarget;
     private Quaternion rotateLerpStart;
     private Quaternion rotateLerpTarget;
+    [SerializeField] private GameObject fadedObject;
 
     private Vector3 TargetPosition
     {
@@ -98,6 +103,26 @@ public class ThirdPersonCameraController : MonoBehaviour
                 rotationTimer = -1;
             }
         }
+
+        if (Physics.Linecast(transform.position, playerController.transform.position, out RaycastHit hitInfo, boundsMask) == true)
+        {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.magenta);
+            if (fadedObject != null && hitInfo.transform.gameObject != fadedObject)
+            {
+                StartCoroutine(FadeObject(fadedObject));
+                Debug.Log("Fading current object in");
+            }
+            else if (fadedObject != hitInfo.transform.gameObject)
+            {
+                StartCoroutine(FadeObject(hitInfo.transform.gameObject));
+                Debug.Log("Fading object out");
+            }
+        }
+        else if (fadedObject != null)
+        {
+            StartCoroutine(FadeObject(fadedObject));
+            Debug.Log("Fading current object in");
+        }
     }
 
     private void MoveCameraToTarget()
@@ -115,5 +140,32 @@ public class ThirdPersonCameraController : MonoBehaviour
         rotateLerpStart = transform.rotation;
         rotateLerpTarget = TargetRotation;
         rotationTimer = 0;
+    }
+
+    private IEnumerator FadeObject(GameObject fadeObject)
+    {
+        if(fadeObject.TryGetComponent(out MeshRenderer renderer) == true)
+        {
+            Color fadeColour = renderer.material.color;
+            float alphaStart = renderer.material.color.a;
+            float alphaEnd = 0;
+            if (fadedObject == fadeObject)
+            {
+                alphaEnd = 1;
+                fadedObject = null;
+            }
+            else
+            {
+                fadedObject = fadeObject;
+            }
+            float fadeTimer = 0;
+            while(fadeTimer < fadeTime)
+            {
+                fadeTimer += Time.deltaTime;
+                fadeColour.a = Mathf.Lerp(alphaStart, alphaEnd,  fadeTimer / fadeTime);
+                renderer.material.color = fadeColour;
+                yield return null;
+            }
+        }
     }
 }
